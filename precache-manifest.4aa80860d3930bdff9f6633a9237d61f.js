083129dda15aa56cbf1ed2268be11d3ec9a5c6a1
George <George.phm@hotmail.com>
2021-09-26 12:00:53 +0400

    Updates

---
+++ precache-manifest.4aa80860d3930bdff9f6633a9237d61f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc81a0d506d1feaac8a7",
+    "url": "/calc/static/css/main.799c5671.chunk.css"
+  },
+  {
+    "revision": "fc81a0d506d1feaac8a7",
+    "url": "/calc/static/js/main.fc81a0d5.chunk.js"
+  },
+  {
+    "revision": "455d3a4716702d5cd61c",
+    "url": "/calc/static/js/1.455d3a47.chunk.js"
+  },
+  {
+    "revision": "117ab63626df48b19938",
+    "url": "/calc/static/js/runtime~main.117ab636.js"
+  },
+  {
+    "revision": "cbd655d1eeabd42fb55a1ac01a3b1637",
+    "url": "/calc/index.html"
+  }
+];
